fc634068f04e873c21cd3bf120630a6a6f3506a4
tetetratra <44367208+tetetratra@users.noreply.github.com>
2019-11-06 22:58:56 +0900

    imageのバグ直し

--- exec.rb
+++ exec.rb
@@ -7,7 +7,6 @@ access_token_secret = ENV['BIOTTER_ACCESS_TOKEN_SECRET']
 handler = Handler.new(consumer_key, consumer_secret, access_token, access_token_secret)
 
 follower_profiles = handler.fetch_follower_profiles
-#updated_follower_profiles = handler.select_updated_follower_profiles(follower_profiles)
-updated_follower_profiles = follower_profiles
+updated_follower_profiles = handler.select_updated_follower_profiles(follower_profiles)
 handler.create_follower_profiles(updated_follower_profiles)
 handler.tweet_follower_profiles(updated_follower_profiles)
