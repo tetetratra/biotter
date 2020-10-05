87f91be0e98150cece4fbde7ba56964d51f11000
tetetratra <44367208+tetetratra@users.noreply.github.com>
2020-10-05 12:59:35 +0900

    画像をDBに入れるスクリプト修正

--- script/insert_all_profile_images.rb
+++ script/insert_all_profile_images.rb
@@ -1,9 +1,13 @@
 require_relative './../common.rb'
 
 Profile.where('user_profile_image_url IS NOT NULL').each do |profile|
-  image_path = './../images/' + profile.user_twitter_id.to_s + '/' + profile.user_profile_image_url.gsub('/', '')
-  image = File.open(image_path)
+  image_path = File.expand_path('../public/images/' + profile.user_twitter_id.to_s + '/' + profile.user_profile_image_url.gsub('/', ''), __dir__)
+  begin
+    image = File.open(image_path)
+  rescue
+    next
+  end
   profile.user_profile_image = image.read
-  profile.save!
+  profile.save
 end
 
